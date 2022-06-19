80328024539d1419bc0476a0478957099787ee4c
aom31 <97218117+aom31@users.noreply.github.com>
2022-06-19 14:14:44 +0700

    up

---
+++ demo/src/main/java/com/example/demo/controller/TestApi.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/testapi")
+public class TestApi {
+
+    @GetMapping("/view")
+    public String viewData(){
+        return "test getmap";
+
+    }
+
+
+}
